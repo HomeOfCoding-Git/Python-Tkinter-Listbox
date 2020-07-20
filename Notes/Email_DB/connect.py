a442c575a8f2ad0c4fc632dfe5ad0c3843d1a9d9
Home Of Coding <HomeOfCoding@outlook.com>
2020-07-20 16:01:25 +0100

    Update connect.py

--- Notes/Email_DB/connect.py
+++ Notes/Email_DB/connect.py
@@ -1,2 +1,4 @@
-EMAIL_USER = ''
-EMAIL_PASS = ''
+# Add your preferred email address and password..
+# Of which you wish to send your emails from.
+EMAIL_USER = 'me@my_email_address.com'
+EMAIL_PASS = 'my_email_password'
